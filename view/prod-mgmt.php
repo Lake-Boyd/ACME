9c8226bcd471f9628e144c916d366c4353e1a5d3
Boyd <Boyd@Boyd-PC.attlocal.net>
2017-02-09 22:26:03 -0600

    first commit

--- view/prod-mgmt.php
+++ view/prod-mgmt.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <!--
-This page is for managing the product database.
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
 -->
     <head>
         <meta charset="UTF-8">
-        <title>ACME Registration</title>
+        <title>ACME Product Mgmt</title>
             <meta name="author" content="Boyd Lake">
-            <meta name="description" content="This page is the product management page for ACME Inc.">
+            <meta name="description" content="This page is the Product Management page for ACME Inc.">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css" />
             <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>	
@@ -33,69 +35,21 @@ This page is for managing the product database.
             
             <main class="maincontent">
 
-                <h1 class="login-title">Register a new account.</h1>
+                <h1 class="login-title">Manage Products</h1>
                 <section class="login-buttons">
-                    <h3>Please fill in the fields below.</h3>
-                    
-                    <?php
-                        if (isset($message)) {
-                         echo $message;
-                        }
-                        ?>
                     
                     
-                    <form method="post" action="/acme/products/index.php" id="addform">
-
-                        <!-- <label for="invid">Inventory Id</label> -->
-                        <input type="hidden" name="invid" id="invid" value="">
-                        
-                        <label for="invname">Inventory Name</label>
-                        <input type="text" name="invname" id="invname">
-                        
-                        <label for="invdescription">Inventory Description</label>
-                        <textarea name="invdescription" id="invdescription" form="addform"></textarea>
-                        
-                        <label for="invimage">Inventory Image</label>
-                        <input type="text" name="invimage" id="invimage" >
-
-                        <label for="invthumbnail">Inventory Thumbnail</label>
-                        <input type="text" name="invthumbnail" id="invthumbnail" >
-                        
-                        <label for="invprice">Inventory Price</label>
-                        <input type="text" name="invprice" id="invprice" >                        
-                        
-                        <label for="invstock">Inventory Stock</label>
-                        <input type="text" name="invstock" id="invstock" >
-
-                        <label for="invsize">Inventory Size</label>
-                        <input type="text" name="invsize" id="invsize" >
-
-                        <label for="invweight">Inventory Weight</label>
-                        <input type="text" name="invweight" id="invweight" >
-
-                        <label for="invlocation">Inventory Location</label>
-                        <input type="text" name="invlocation" id="invlocation" >
-
-                        <label for="categoryid">Category Id</label>
-                        
-                        <?php
-                        
-                            echo $prodcatList ;
-                        
-                        ?>
-                        
-                        <input type="text" name="categoryid" id="categoryid" >
-
-                        <label for="invvendor">Inventory Vendor</label>
-                        <input type="text" name="invvendor" id="invvendor" >
-
-                        <label for="invstyle">Inventory Style</label>
-                        <input type="text" name="invstyle" id="invstyle" >                        
-                        
-                        <button type="submit" name="addprod" id="addprod">Submit Product</button>
-                        <!-- Add the action key - value pair -->
-                        <input type="hidden" name="action" value="addproduct">
-                    </form>
+<!--                    <form method="post" action="/acme/accounts/index.php">
+                         <label for="username">User name</label> 
+                        <input type="text" name="username" id="username" placeholder="Username" required>
+                         <label for="email">Email</label> 
+                        <input type="email" name="email" id="email" placeholder="Email" required>
+                        <input type="submit" value="Sign In" id="sign-in">
+                    </form>-->
+                    <ul>    
+                        <li><a href='.?action=newprod' title="New Product">Add a new product</a></li>
+                        <li><a href='.?action=newcat' title="New Category">Add a new category</a></li>
+                    </ul>
 
                 </section>
 
